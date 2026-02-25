502f0f26eb06e1a78c80b221f6f04d6507d61956
Alex <zigzaksasha@gmail.com>
2026-02-25 21:18:18 +0100

    Solving problem

--- app/main.py
+++ app/main.py
@@ -26,42 +26,32 @@ def shop_trip():
         for shop in shops:
             total_trip_cost = customer.calc_trip_cost(shop, fuel_price)
             trip_costs.append((total_trip_cost, shop))
-
-            print(f"{customer.name}'s trip to the {shop.name}"
-                  f" costs {total_trip_cost:.2f}")
+            print(f"{customer.name}'s trip to the {shop.name} costs {total_trip_cost:.2f}")
 
         best_cost, best_shop = min(trip_costs, key=lambda x: x[0])
 
         if best_cost <= customer.money:
-            print(f"{customer.name} rides to {best_shop.name}")
+            print(f"{customer.name} rides to {best_shop.name}\n")
             customer.location = best_shop.location
 
-            print(f"Date: {datetime.datetime.now().strftime(
-                '%d/%m/%Y %H:%M:%S')}")
+            print(f"Date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
             print(f"Thanks, {customer.name}, for your purchase!")
             print("You have bought:")
-            product_total = 0
-            for item, quantity in customer.shopping_cart.items():
+
+            product_total = best_shop.total_products_cost(customer.product_cart)
+
+            for item, quantity in customer.product_cart.items():
                 price_per_unit = best_shop.products[item]
                 item_total = quantity * price_per_unit
-                product_total += item_total
 
-                unit_word = "dollars" if item_total != 1 else "dollar"
-                print(f"{quantity} {item}s for {item_total} {unit_word}")
+                print(f"{quantity} {item}s for {item_total:g} dollars")
 
-            print(f"Total cost is {product_total:.2f} dollars")
-            print("See you again!")
-
-            print(f"Total cost is {best_cost:.2f} dollars")
+            print(f"Total cost is {product_total} dollars")
+            print("See you again!\n")
 
             print(f"{customer.name} rides home")
             customer.location = home_location
             customer.money -= best_cost
-            print(f"{customer.name} now has {customer.money:.2f} dollars\n")
+            print(f"{customer.name} now has {customer.money:g} dollars")
         else:
-            print(f"{customer.name} doesn't have enough"
-                  f" money to make a purchase in any shop\n")
-
-
-if __name__ == "__main__":
-    shop_trip()
+            print(f"{customer.name} doesn't have enough money to make a purchase in any shop")
